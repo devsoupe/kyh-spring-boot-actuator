e8113d015b6fac612cf0b2e1830b02948d716944
devsoupe <luvyum4e@gmail.com>
2023-05-16 00:17:37 +0900

    HTTP 요청 응답 기록

--- src/main/java/hello/KyhSpringBootActuatorApplication.java
+++ src/main/java/hello/KyhSpringBootActuatorApplication.java
@@ -1,7 +1,9 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KyhSpringBootActuatorApplication {
@@ -10,4 +12,8 @@ public class KyhSpringBootActuatorApplication {
         SpringApplication.run(KyhSpringBootActuatorApplication.class, args);
     }
 
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
